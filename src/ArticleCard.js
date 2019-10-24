acfb5146430e9c12d4f92047e0a1a629197c1cd5
Piotr Napieralski <piotrn-87@o2.pl>
2019-10-24 21:39:52 +0200

    Feat: Added some content to CardContent

--- src/ArticleCard.js
+++ src/ArticleCard.js
@@ -1,5 +1,16 @@
 import React from "react";
-import { Button, CardHeader, Card, Avatar, CardMedia } from "@material-ui/core";
+import {
+  Button,
+  CardHeader,
+  Card,
+  Avatar,
+  CardMedia,
+  CardActions,
+  IconButton,
+  Icon,
+  CardContent,
+  Typography
+} from "@material-ui/core";
 
 function ArticleCard(props) {
   return (
@@ -10,7 +21,26 @@ function ArticleCard(props) {
         subheader="2019-10-22"
       ></CardHeader>
       <CardMedia image="http://picsum.photos/id/1015/600/400"></CardMedia>
-      <Button variant="contained">Read more</Button>
+      <CardContent>
+        <Typography variant="h6">
+          Początkujący programista z wielkim zapałem do pracy i jeszcze większą
+          checią poznawania nowych technologii. Jada zbyt kalorycznie, często
+          podejmuje zle decyzje, zdarza mu się nie mówić ładnie. Nie lubi
+          poranków i nie oglada Netflixa. Uwielbia bliski kontakt z naturą oraz
+          zapach kawy o poranku.
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton>
+          <Icon>favorite</Icon>
+        </IconButton>
+        <IconButton>
+          <Icon>share</Icon>
+        </IconButton>
+      </CardActions>
+      <Button variant="contained" color="primary">
+        Read more
+      </Button>
     </Card>
   );
 }
