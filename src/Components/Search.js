b5fbbfd991a9f2c11035e267a6f03e3252ce32ec
Piotr Napieralski <piotrn-87@o2.pl>
2019-11-24 19:00:22 +0100

    Feat: Training

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
 
 class Search extends Component {
   constructor() {
@@ -10,15 +11,28 @@ class Search extends Component {
       amount: "",
       loading: true,
       person: null,
-      searchText: ""
+      searchText: "",
+      images: []
     };
   }
   async componentDidMount() {
-    const url = "https://api.randomuser.me/";
+    // const url = "https://api.randomuser.me/";
+    const url = "https://api.github.com/users/piotrn-87/repos";
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-    this.setState({ person: data.results[0], loading: false });
+
+    // fetch("https://api.github.com/users/piotrn-87/repos")
+    //   .then(resp => resp.json)
+    //   .then(resp => {
+    //     const repository = resp;
+    //     for (const repo of repository) {
+    //       const { name, description, html_url } = repo;
+    //     }
+    //   });
+
+    // this.setState({ person: data.results[0], loading: false });
+    this.setState({ person: data[11], loading: false });
+    console.log(this.state.person);
   }
 
   onAmountChange = (e, index, value) => {
@@ -35,7 +49,7 @@ class Search extends Component {
           value={this.state.searchText}
           onChange={this.onTextChange}
           floatingLabelText="github repository"
-          fullWidth={true}
+          // fullWidth={true}
         />
         <br />
         <SelectField
@@ -43,7 +57,6 @@ class Search extends Component {
           floatingLabelText="Amount"
           value={this.state.amount}
           onChange={this.onAmountChange}
-          fullWidth={false}
         >
           <MenuItem value={5} primaryText="5" />
           <MenuItem value={10} primaryText="10" />
@@ -54,7 +67,9 @@ class Search extends Component {
         </SelectField>
         <br />
 
-        <div>
+        <div>wait</div>
+
+        {/* <div>
           {this.state.loading || !this.state.person ? (
             <div>loading ... </div>
           ) : (
@@ -63,12 +78,29 @@ class Search extends Component {
               <h1>{this.state.person.name.first}</h1>
               <h1>{this.state.person.name.last}</h1>
               <img src={this.state.person.picture.large} />
+              <h1>{this.state.person.location.city}</h1>
+              <h1>{this.state.person.location.country}</h1>
             </div>
           )}
-        </div>
+        </div> */}
       </div>
     );
   }
 }
 
 export default Search;
+
+// onTextChange = e => {
+//   const val = e.target.value;
+//   this.setState({ [e.target.name]: val }, () => {
+//     if (val === "") {
+//       this.setState({ images: [] });
+//     } else {
+//       axios
+//         .get(`https://api.github.com/users/${this.state.searchText}/repos`)
+//         .then(res => this.setState({ images: res.data.hits }))
+//         .catch(err => console.log(err));
+//     }
+//     console.log("log", this.state.images);
+//   });
+// };
