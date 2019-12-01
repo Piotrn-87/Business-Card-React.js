613ba097b11c54711a85afdd54c166fdbcaf3890
Piotr Napieralski <piotrn-87@o2.pl>
2019-12-01 20:53:51 +0100

    Updates

---
+++ precache-manifest.ddbfaaa54d2b25e3f3e461273c903ca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25a402ff56d75cf8255889d4409211d",
+    "url": "/Business-Card-React.js/index.html"
+  },
+  {
+    "revision": "0711f17b08b08e959d13",
+    "url": "/Business-Card-React.js/static/css/main.919bc91d.chunk.css"
+  },
+  {
+    "revision": "769b2802746ab3bb094a",
+    "url": "/Business-Card-React.js/static/js/2.2d143423.chunk.js"
+  },
+  {
+    "revision": "0711f17b08b08e959d13",
+    "url": "/Business-Card-React.js/static/js/main.f69043ef.chunk.js"
+  },
+  {
+    "revision": "23e576ffac878aa2280b",
+    "url": "/Business-Card-React.js/static/js/runtime-main.3aee7266.js"
+  },
+  {
+    "revision": "5f5e3d9ae284b236ec2c64b060aa7893",
+    "url": "/Business-Card-React.js/static/media/code.5f5e3d9a.jpg"
+  }
+]);
